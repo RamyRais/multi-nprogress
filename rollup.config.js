711815730219074262b6a5f6112dc9ac5ded6fc0
Ramy Rais <ramy.rais@gmail.com>
2025-07-06 22:01:02 +0200

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'nprogress.js',
+  input: 'src/nprogress.js',
   output: [
     {
       file: 'dist/nprogress.js',
